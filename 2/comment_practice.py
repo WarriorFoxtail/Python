570dff69f2816cc05ec708d4fd9737d0014e910b
WarriorFoxtail <mgcook1998@gmail.com>
2024-02-15 11:34:22 -0600

    reorganizing

---
+++ 2/comment_practice.py
@@ -0,0 +1,11 @@
+#program to determine gas milage of a tank
+
+# variables
+tank_size = 15 # size of tank
+distance = 600 # distance traveled
+
+# calculation
+mpg = distance / tank_size
+
+# outputting results
+print("Miles per gallon:", mpg)
