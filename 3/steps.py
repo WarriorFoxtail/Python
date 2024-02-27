18cbb028c428dbf81564fbef01f5db221a3630a6
WarriorFoxtail <mgcook1998@gmail.com>
2024-02-26 22:49:08 -0600

    steps notes

--- 3/steps.py
+++ 3/steps.py
@@ -6,13 +6,15 @@ steps=[]
 total=0
 for day in days:
     steps_taken=int(input(f"How many steps did you take on {day}? "))
-    steps.append(steps_taken)
-    total+=steps_taken
+    steps.append(steps_taken) #adding each input to the list of steps taken
+    total+=steps_taken #adding the total steps taken each day to the total of steps taken overall
 
+#displaying how many steps the user took each day
 index=0
 for day in days:
     print(f"On {day} you took {steps[index]}")
     index+=1
 
+#computing the average of steps taken per day
 average=total/len(steps)
 print(f"You walked an average of {average:.1f} steps per day.")
