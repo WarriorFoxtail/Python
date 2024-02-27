dbdf182e9db9a60fe7654883aa322e9de560edf7
WarriorFoxtail <mgcook1998@gmail.com>
2024-02-26 21:46:28 -0600

    log ops

---
+++ 3/log_op_int.py
@@ -0,0 +1,9 @@
+# Program to compare two integers using basic logic operators
+
+# Getting user input
+a = int(input("Please enter an integer: "))
+b = int(input("Please enter another integer: "))
+
+# AND operator
+if a > 0 and b > 0:
+    
