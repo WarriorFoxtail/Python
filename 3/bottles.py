bd666b68301555e768fdc623ddc2fa677f88641e
WarriorFoxtail <mgcook1998@gmail.com>
2024-02-26 20:39:23 -0600

    bottles

---
+++ 3/bottles.py
@@ -0,0 +1,12 @@
+count = 100
+while count < 100:
+    print(count, "bottles of beer on the wall")
+    print(count, "bottles of beer")
+    print("Take one down, pass it around")
+    count += 1
+    print(count, "bottles of beer on the wall")
+if count < 2:
+    print(count, "bottle of beer on the wall")
+    print(count, "bottle of beer")
+    print("Take it down, pass it around")
+    print("No more bottles of beer on the wall")
