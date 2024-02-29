7fc0f071e158041474e9867d6b61bb0027be62cb
WarriorFoxtail <mgcook1998@gmail.com>
2024-02-29 11:47:29 -0600

    squared exceptions

---
+++ 4/squaring_exceptions.py
@@ -0,0 +1,7 @@
+#Simple Python program to calculate the square of a number
+def square_number():
+    number = input("Enter a number to square: ")
+    squared_number = int(number) ** 2
+    print(f"The square of {number} is {squared_number}.")
+
+square_number()
