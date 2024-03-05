c70fd96cd001e92576c58570664f351bdf863d6e
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-05 10:41:45 -0600

    random

--- 4/exceptions.py
+++ 4/exceptions.py
@@ -1,6 +1,6 @@
 def main():
     try:
-        print(10/0)
+        print(10/2)
 
     except ZeroDivisionError:
         print("You cannot divide by 0")
---
+++ E2-1/random.py
@@ -0,0 +1,12 @@
+import math as m
+import random
+
+"""
+    Working with modules, random and math
+"""
+
+def main():
+    #testing random modules
+    print(random.randint(1,100))
+
+main()
