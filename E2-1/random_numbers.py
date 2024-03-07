34ae46c96bd91c7407895f5fb82cdc95f65343f6
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-07 10:41:03 -0600

    formatting

--- E2-1/random_numbers.py
+++ E2-1/random_numbers.py
@@ -1,34 +1,35 @@
 import random
-#Working with modules, random and math
+# Working with modules, random and math
 
 
 def game(win):
-    #getting input, checking for Value Errors, and initializing loop
+    # getting input, checking for Value Errors, and initializing loop
     try:
-        guess=int(input("Please enter a number between 1 and 100: "))
-        distance=win-guess
-        abs_distance=abs(distance)
-        if guess==win:
+        guess = int(input("Please enter a number between 1 and 100: "))
+        distance = win-guess
+        abs_distance = abs(distance)
+        if guess == win:
             print("You guessed the correct number! You're on fire!")
             return
-        elif abs_distance<5:
+        elif abs_distance < 5:
             print("Hot")
-        elif abs_distance<15:
+        elif abs_distance < 15:
             print("Warmer")
-        elif abs_distance<25:
+        elif abs_distance < 25:
             print("Cooler")
         else:
             print("Cold")
         game(win)
-        
+
     except:
         print("Whoops! That isn't a valid value. Please enter an integer.")
-        game(win)        
-        
+        game(win)
+
 
 def main():
-    #testing random modules
-    win=random.randint(1,100)
+    # testing random modules
+    win = random.randint(1, 100)
     game(win)
-    
+
+
 main()
