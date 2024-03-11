f00c700e101bf5a8a43a7d295d0f39f84e75de26
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-11 15:15:00 -0500

    areas

---
+++ 4/calculating_area.py
@@ -0,0 +1,11 @@
+def square(side):
+    square_area=side*side
+    print("The area of the square is "+str(square_area)+" square units.")
+
+def circle(radius):
+    circle_area=3.14*radius**2
+    print("The area of the circle is "+str(circle_area)+" square units.")
+
+square(2)
+
+circle(7)
