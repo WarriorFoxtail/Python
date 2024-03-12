01c7db85b58dcf2fa2938ece08c2c3983fdcbf97
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-12 10:44:44 -0500

    modules

--- modules/main.py
+++ modules/main.py
@@ -2,9 +2,16 @@ from math_operations import calculator
 from math_operations import geometry
 
 result = calculator.add(5, 3)
-print("Addition result:", result)
+print("Addition result: ", result)
 
 result = calculator.subtract(10, 4)
-print("Subtraction result:", result)
+print("Subtraction result: ", result)
 
-result=
+result=geometry.rectangle(4, 6)
+print("Area of Rectangle result: ", result, " square units.")
+
+result=geometry.triangle(7, 9)
+print("Area of Triangle result: ", result, " square units.")
+
+result=geometry.circle(12)
+print("Area of Circle result: ", result, " square units.")
---
+++ modules/math_operations/geometry.py
@@ -0,0 +1,8 @@
+def rectangle(b, h):
+    return b*h
+
+def triangle(b, h):
+    return (b*h)/2
+
+def circle(r):
+    return 3.14*(r*r)
