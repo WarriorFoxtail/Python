e68969f6eaedc74198b04f9bf55dc3d41241d042
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-11 21:14:34 -0500

    dictionaries

--- 4/nato_dictionary.py
+++ 4/nato_dictionary.py
@@ -1,5 +1,19 @@
-alphabet={"a":"apple", "b":"banana", "c":"cucumber", "d":"daikon", "e":"eggplant", "f":"fig", "g":"garlic",
-          "h":"hazelnut", "i":"icaco", "j":"jicama", "k":"kiwi", "l":"lemon", "m":"mango", "n":"nectarine",
-          "o":"🧅", "p":"peach", "q":"quince", "r":"radish", "s":"strawberry", "t":"tomato", "u":"ube", "v":"vanilla",
-          "w":"watermelon", "x":"ximenia", "y":"yam", "z":"zucchini", " ":" ", ".":"."}
+#creating alphabet
+alphabet={"A":"Alyssum", "B":"Bluebell", "C":"Carnation", "D":"Daisy", "E":"Eucalyptus", "F":"Foxglove", "G":"Gardenia",
+          "H":"Hydrangea", "I":"Iris", "J":"Jasmine", "K":"Kalmia", "L":"Lily", "M":"Marigold", "N":"Narcissus",
+          "O":"Orchid", "P":"Peony", "Q":"Quamash", "R":"Rose", "S":"Sunflower", "T":"Tulip", "U":"Ursinia", "V":"Violet",
+          "W":"Wisteria", "X":"Xeranthemum", "Y":"Yarrow", "Z":"Zinnia", " ":" ", ".":"."}
 
+#defining the function
+def get_word():
+    #getting user input
+    user_word=input("Please enter a word: ")
+    for letter in user_word.upper(): #initializes loop and converts word to upper case
+        print(alphabet.get(letter)) #iterates through word, searches for corrosponding keys, and prints the associated value
+
+#calling get_word function
+def main():
+    get_word()
+
+#calling main
+main()
