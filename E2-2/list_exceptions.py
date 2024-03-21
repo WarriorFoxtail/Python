0a02e118fa3fcfa72e4a42dbc9d9ea5e18f09584
WarriorFoxtail <mgcook1998@gmail.com>
2024-03-21 11:16:30 -0500

    list except

---
+++ E2-2/list_exceptions.py
@@ -0,0 +1,6 @@
+def main():
+    top_artists = ["The Beatles", "Madonna", "Elton John", "Elvis Presley", "Mariah Carey", "Stevie Wonder", "Janet Jackson", "Michael Jackson", "Whitney Houston", "Rihanna"]
+    # Your code goes here
+
+
+main()
