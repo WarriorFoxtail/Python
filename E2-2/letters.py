a334f4d03ff21e106a104d9d83197c5ea97033fe
WarriorFoxtail <mgcook1998@gmail.com>
2024-04-16 11:35:10 -0500

    generators and iterators

---
+++ E2-2/letters.py
@@ -0,0 +1,10 @@
+#defining the iterator
+def two_letter_combinations(characters):
+    #implement the generator function
+    
+
+def main():
+    #call the generator function and print each combination
+
+#calling main
+main()
